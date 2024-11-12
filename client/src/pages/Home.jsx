c0b0f2e066a71c4d2965562b71dda9c44d5c6973
vineetsrv <vineetsarovi140@gmail.com>
2024-11-12 19:43:26 +0530

    Initialize project structure and testing done

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div className="p-4 text-center text-xl font-bold text-blue-600">
+        Home
+      </div>
+    </>
+  );
+}
+
+export default Home;
